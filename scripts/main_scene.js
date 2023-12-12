d220593996d7a251a21a2b1ece43111871fbe58f
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2023-12-12 14:37:07 +0900

    新規プロジェクトを作成する

---
+++ scripts/main_scene.js
@@ -0,0 +1,18 @@
+// シーンクラス
+// 他のJSファイルから呼び出された場合はシーンを返す
+class MainScene extends Phaser.Scene {
+    // コンストラクタ
+    constructor() {
+        // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
+        super('MainScene');
+    }
+    // シーンの事前読み込み処理
+    preload() {
+    }
+    // シーン初期化処理
+    create() {
+    }
+    // 毎フレーム実行される繰り返し処理
+    update() {
+    }
+}
