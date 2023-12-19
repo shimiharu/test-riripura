8ecd13424f5bc71b55ff6e4bba458249448326a3
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2023-12-19 14:35:19 +0900

    taroとhanakoを表示し、カーソルキーで移動させたclose #4, #5, #6

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -21,8 +21,32 @@ class MainScene extends Phaser.Scene {
         const hanako = this.physics.add.sprite(750, 400, 'hanako');
         this.taro = taro
         this.hanako = hanako
-        }
+    }
      // 毎フレーム実行される繰り返し処理
-    update(time, delta) {   
+    update() {
+        // キーボードの情報を取得
+        let cursors = this.input.keyboard.createCursorKeys();
+        if(cursors.up.isDown){
+            console.log("Up!!");
+            this.taro.setVelocityY(-40);// 上方向の速度を設定
+            this.hanako.setVelocityY(40);
+        } else if(cursors.down.isDown){
+            console.log("down!!");
+            this.taro.setVelocityY(40);// 下方向の速度を設定
+            this.hanako.setVelocityY(-40);
+        }else if(cursors.left.isDown){
+            console.log("Left");
+            this.taro.setVelocityX(-40);// 左方向の速度を設定
+            this.hanako.setVelocityX(40);
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            this.taro.setVelocityX(40);// 右方向の速度を設定
+            this.hanako.setVelocityX(-40);
+        }else{
+            this.taro.setVelocityX(0);// 横方向の速度を0
+            this.hanako.setVelocityX(0);
+            this.taro.setVelocityY(0);// 縦方向の速度を0
+            this.hanako.setVelocityX(0);
+        }
     }
 }
