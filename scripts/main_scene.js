cbca8c0a6134e0160096c3ba696480c562f45811
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2023-12-19 14:17:25 +0900

    taroとhanakoを表示した

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -10,11 +10,17 @@ class MainScene extends Phaser.Scene {
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
+        this.load.image('hanako', 'assets/hanako.png');
     }
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'background');
+        const taro = this.physics.add.sprite(50, 50, 'taro');
+        const hanako = this.physics.add.sprite(750, 400, 'hanako');
+        this.taro = taro
+        this.hanako = hanako
         }
      // 毎フレーム実行される繰り返し処理
     update(time, delta) {   
