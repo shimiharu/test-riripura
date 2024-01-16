cf049479155801d1b4cc0e070d19eefcb66cd61a
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2024-01-16 14:03:50 +0900

    hanakoの当たり判定追加

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -22,21 +22,30 @@ class MainScene extends Phaser.Scene {
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'background');
-        
-
         const taro = this.physics.add.sprite(50, 50, 'taro')
         const hanako = this.physics.add.sprite(750, 400, 'hanako')
+        let staticGroup = this.physics.add.staticGroup();
         for(let i=0; i<5; i++){
         let  randx = Phaser.Math.Between(25, 775) ;  // y は　50～750の間の値
         let randy =  Phaser.Math.Between(25, 425) ;  // y は　50～200の間の値
-        this.add.image(randx, randy , 'orange'); }//ランダムな場所に生成
+        staticGroup.create(randx, randy , 'orange'); }//ランダムな場所に生成
         for(let i=0; i<5; i++){
         let  randa = Phaser.Math.Between(25, 775) ;  // y は　50～750の間の値
         let randb =  Phaser.Math.Between(25, 425) ;  // y は　50～200の間の値
-        this.add.image(randa, randb , 'apple');} //ランダムな場所に生成
+        staticGroup.create(randa, randb , 'apple');} //ランダムな場所に生成
         this.taro = taro
         this.hanako = hanako
 
+        
+
+        this.physics.add.overlap(taro, staticGroup, collectf, null, this);
+        function collectf(){
+        this.physics.pause(this.add.text(D_WIDTH/3,D_HEIGHT*1/3, 'Game Over!', { fontSize: '32px', fill: '#CDC' }));
+        }
+        this.physics.add.overlap(hanako, staticGroup, collectf, null, this);
+        function collectf(){
+        this.physics.pause(this.add.text(D_WIDTH/3,D_HEIGHT*1/3, 'Game Over!', { fontSize: '32px', fill: '#CDC' }));
+        }
         
         }
      // 毎フレーム実行される繰り返し処理
@@ -66,4 +75,5 @@ class MainScene extends Phaser.Scene {
              this.hanako.setVelocityY(0);// 縦方向の速度を0
          }
     }
+
 }
